148daa848e7bbe4a8ca8972c86f1f6d3d26a76ae
peris <964480899@qq.com>
2022-08-11 23:57:43 +0800

    Promise(穿透、返回一个成功/失败返回promise 处理)

---
+++ JavaScript/手写Promise/index.01.js
@@ -0,0 +1,35 @@
+let p1 = Promise.resolve(1)
+let p2 = Promise.resolve(2)
+let p3 = Promise.reject(3)
+let p4 = Promise.reject(4)
+let arr = [p1, p2, p3, p4]
+Promise.all(arr).then(
+  (res) => {
+    console.log('resolve')
+    console.log(res)
+  },
+  (reason) => {
+    console.log('reject')
+    console.log(reason)
+  }
+)
+
+function all(arr) {
+  let t_i = 0
+  let t_res = []
+  let rej
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].then(
+      (res) => {
+        t_i++
+        t_res.push(res)
+      },
+      (reason) => {
+        rej = reason
+      }
+    )
+    if (rej) break
+  }
+  if (rej) return reject(rej)
+  resolve(t_res)
+}
